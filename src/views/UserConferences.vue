8ccc71bf468ea67ffd7c22716bf8ff0680b96e8b
mateusz5564 <mateusz5564@gmail.com>
2019-09-25 14:59:04 +0200

    uzycie komponentu miniatury konferencji w widokach, dodanie przycisku edycji przy uzyciu slota w widoku konferencji uzytkownika

--- src/views/UserConferences.vue
+++ src/views/UserConferences.vue
@@ -3,34 +3,52 @@
     <h2 class="mb-6 headline font-weight-black" >Moje konferencje</h2>
     <v-row>
       <v-col v-for="(conference, index) in conferences" :key="index" cols="12" xs="12" md="6" xl="4">
-        <!-- <v-card class="mx-auto">
-          <v-list-item>
-            <v-list-item-content>
-              <h2 class="headline font-weight-medium pb-4 pt-4">{{conference.title}}</h2>
-              <p class="pb-2 ">{{conference.start_date.toDate() | dateFilter}} - {{conference.end_date.toDate() | dateFilter}}</p>
-              <v-divider class="blue lighten-1"></v-divider>
-              <div class="pt-2 pb-2 body-1 font-weight-medium">
-              <v-icon>mdi-map-marker</v-icon> {{ conference.location }}
-              <v-spacer></v-spacer>
-              <a :href="conference.link" target="_blank">zobacz na mapie</a>
-              </div>
-            </v-list-item-content>
-          </v-list-item>
-        
-          <v-img :src="conference.logo" height="294"></v-img>
-
-         <v-card-text class="headline mb-5">
-            <div class="text--primary">
-              {{conference.description}}
-            </div>
-          </v-card-text> 
-
-          <v-card-actions>
-            <div class="flex-grow-1"></div>
-            <v-btn text color="blue accent-4">więcej informacji</v-btn>
-          </v-card-actions>
-        </v-card> -->
+        <ConferenceThumbnail :conference="conference">
+          <div class="pb-4" slot="floating-btn">
+            <v-btn fab color="orange accent-2" top right absolute>
+              <v-icon>mdi-pencil</v-icon>
+            </v-btn>
+          </div>
+        </ConferenceThumbnail>
       </v-col>
     </v-row>
   </v-container>
 </template>
+
+<script>
+import ConferenceThumbnail from '@/components/ConferenceThumbnail.vue'
+import db from '@/firebase/init'
+import firebase from 'firebase'
+import axios from 'axios'
+
+export default {
+  components: {
+    ConferenceThumbnail
+  },
+  data(){
+    return {
+      conferences: []
+    }
+  },
+  created(){
+     db.collection('conferences')
+        .get()
+        .then(querySnapshot => {
+          querySnapshot.forEach(doc => {
+             axios.post('https://maps.googleapis.com/maps/api/geocode/json?latlng=' + doc.data().location.latitude + ',' + doc.data().location.longitude + '&key=AIzaSyDtYbZokAi1OVXplmLIpuxlJpppE0fijPA')
+              .then(response => {
+                let address = response.data.results[0].formatted_address
+                let dataRef = doc.data()
+                dataRef.location = address
+                const link = `https://maps.google.com/?q=${address}`
+                dataRef.link = link
+                this.conferences.push(dataRef)
+              })
+              .catch(e => {
+                console.log(e)
+            })
+          })
+        })
+  }
+}
+</script>
