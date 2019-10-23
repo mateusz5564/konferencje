4e038c7cb6e137e8a5eb03c707d8c0520abffac3
mateusz5564 <mateusz5564@gmail.com>
2019-10-23 21:25:20 +0200

    podstawa layoutu profilu

--- src/views/UserProfile.vue
+++ src/views/UserProfile.vue
@@ -1,9 +1,49 @@
 <template>
-  <div class="user-profile">
-    <v-card v-if="profile" >
-      {{ profile.username }}
+  <div class="user-profile mx-auto">
+    <v-card max-width="800px" v-if="profile" >
+      <h2>{{ profile.username }}</h2>
     </v-card>
     
+ <v-card max-width="800px">
+
+    <v-tabs vertical>
+      <v-tab>
+        <v-icon left>mdi-account</v-icon>
+        Edytuj profil
+      </v-tab>
+      <v-tab>
+        <v-icon left>mdi-lock</v-icon>
+        Zmień hasło
+      </v-tab>
+      <v-tab>
+        <v-icon left>mdi-access-point</v-icon>
+        Opcja 3333
+      </v-tab>
+
+      <v-tab-item>
+        <v-card flat>
+          <v-card-text>
+            estestestes
+          </v-card-text>
+        </v-card>
+      </v-tab-item>
+      <v-tab-item>
+        <v-card flat>
+          <v-card-text>
+            etsetestes
+          </v-card-text>
+        </v-card>
+      </v-tab-item>
+      <v-tab-item>
+        <v-card flat>
+          <v-card-text>
+            testestestes
+          </v-card-text>
+        </v-card>
+      </v-tab-item>
+    </v-tabs>
+  </v-card>
+
   </div>
 </template>
 
